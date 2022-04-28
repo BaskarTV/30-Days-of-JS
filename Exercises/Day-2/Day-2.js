c3d548d848ce0ce7871012a23e762551d31a937c
BASKAR T <baskar.tece@gmail.com>
2022-04-29 02:00:30 +0530

    Day 2 completed

---
+++ Exercises/Day-2/Day-2.js
@@ -0,0 +1,230 @@
+//  💻 Day 2: Exercises
+
+
+//************* */ Exercise: Level 1********************
+
+// Question-1 Declare a variable named challenge and assign it to an initial value '30 Days Of JavaScript'.
+
+let challenge = '30 Days of JavaScript';
+
+// Question-2 Print the string on the browser console using console.log()
+console.log(challenge)
+
+//Questio-3 Print the length of the string on the browser console using console.log()
+console.log(challenge.length); 
+
+// Question-4 Change all the string characters to capital letters using toUpperCase() method
+
+console.log(challenge.toUpperCase())
+
+// Question-5 Change all the string characters to lowercase letters using toLowerCase() method
+
+console.log(challenge.toLowerCase());
+
+// Question-6 Cut (slice) out the first word of the string using substr() or substring() method
+
+console.log(challenge.substr(0,2))
+
+// Question-7 Slice out the phrase Days Of JavaScript from 30 Days Of JavaScript.
+
+console.log(challenge.substr(3,18))
+
+// Question-8 Check if the string contains a word Script using includes() method
+
+console.log(challenge.includes('Script'))
+
+// Question-9 Split the string into an array using split() method
+
+console.log(challenge.split())
+
+// Question-10 Split the string 30 Days Of JavaScript at the space using split() method
+
+console.log(challenge.split(' '))
+
+// Question-11 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon' split the string at the comma and change it to an array.
+
+
+let techCompanies = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon' 
+
+console.log(techCompanies.split(","))
+
+// Question-12 Change 30 Days Of JavaScript to 30 Days Of Python using replace() method.
+
+console.log(challenge.replace('JavaScript','Python'))
+
+// Question-13 What is character at index 15 in '30 Days Of JavaScript' string? Use charAt() method.
+
+console.log(challenge.charAt(15))
+
+// Question-14 What is the character code of J in '30 Days Of JavaScript' string using charCodeAt()
+
+console.log(challenge.charCodeAt('J'))
+
+// Question-15 Use indexOf to determine the position of the first occurrence of a in 30 Days Of JavaScript
+
+console.log(challenge.indexOf('a'))
+
+// Question-16 Use lastIndexOf to determine the position of the last occurrence of a in 30 Days Of JavaScript.
+
+console.log(challenge.lastIndexOf('a'))
+
+// Question-17 Use indexOf to find the position of the first occurrence of the word because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
+
+let sentence = 'You cannot end a sentence with because because because is a conjunction';
+
+console.log(sentence.indexOf('because'))
+
+// Question-18 Use lastIndexOf to find the position of the last occurrence of the word because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
+
+console.log(sentence.lastIndexOf('because'))
+
+// Question-19 Use search to find the position of the first occurrence of the word because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
+
+console.log(sentence.search('because'))
+
+// Question-20 Use trim() to remove any trailing whitespace at the beginning and the end of a string.E.g ' 30 Days Of JavaScript '.
+
+console.log(challenge.trim());
+
+// Question-21 Use startsWith() method with the string 30 Days Of JavaScript and make the result true
+
+console.log(challenge.startsWith(30))
+
+// Question-22 Use endsWith() method with the string 30 Days Of JavaScript and make the result true
+
+console.log(challenge.endsWith('Script'))
+
+// Question-23 Use match() method to find all the a’s in 30 Days Of JavaScript
+
+console.log(challenge.match(/a/gi))
+console.log(challenge.match(/s/gi))
+console.log(challenge.match(/f/gi))
+
+// Question-24 Use concat() and merge '30 Days of' and 'JavaScript' to a single string, '30 Days Of JavaScript'
+
+let firstString = '30 Days of';
+let secondString='JavaScript';
+
+console.log(firstString.concat(" ",secondString))
+
+// Question-25 Use repeat() method to print 30 Days Of JavaScript 2 times
+
+console.log(challenge.repeat(2))
+
+//*************** */ Exercise: Level 2*****************
+
+// Question-1 Using console.log() print out the following statement:
+
+console.log('The quote \'There is no exercise better for the heart than reaching down and lifting people up.\' \ by John Holmes teaches us to help one another.\ ')
+
+// Question-2  Using console.log() print out the following quote by Mother Teresa:
+
+console.log("Love is not patronizing and charity isn\'t about pity, it is about love. Charity and love are the same -- with charity you give love\, so don't just give money but reach out your hand instead."
+,"By Mother Teresa");
+
+// Question-3 Check if typeof '10' is exactly equal to 10. If not make it exactly equal.
+
+let  num1 =10;
+let num2 = '10'
+
+console.log(typeof(num1))
+console.log(typeof(num2))
+
+console.log(parseInt(num2))
+console.log(num2)
+
+//Question-4 Check if parseFloat('9.8') is equal to 10 if not make it exactly equal with 10.
+
+let parseFloat = 9.8
+console.log(parseFloat)
+console.log(Math.round(parseFloat));
+
+// Question-5 Check if 'on' is found in both python and jargon
+
+let py = 'python';
+let ja = 'jargon';
+
+console.log(py.match(/on/gi))
+console.log(ja.match(/on/gi))
+
+// Question-6 I hope this course is not full of jargon. Check if jargon is in the sentence.
+
+let findSen = 'I hope this course is not full of jargon.'
+
+console.log(findSen.search('Jargon'))
+
+
+// Question-7 Generate a random number between 0 and 100 inclusively.
+
+console.log(Math.floor(Math.random()*101))
+
+// Question-8 Generate a random number between 50 and 100 inclusively.
+
+console.log(Math.floor(Math.random()* 51 ) + 50)
+
+// Question-9 Generate a random number between 0 and 255 inclusively.
+
+console.log(Math.floor(Math.random()* 256));
+
+// Question-10 Access the 'JavaScript' string characters using a random number
+
+console.log('JavaScript'.charAt(Math.random()*11))
+
+// Question-11 Use console.log() and escape characters to print the following pattern.
+
+/*1 1 1 1 1
+   2 1 2 4 8
+   3 1 3 9 27
+  4 1 4 16 64
+  5 1 5 25 125 */
+
+
+console.log("1 1 1 1 1\n2 1 2 4 8\n3 1 3 9 27\n4 1 4 16 64\n5 1 5 25 125")
+
+
+// Question-12 Use substr to slice out the phrase because because because from the following sentence:'You cannot end a sentence with because because because is a conjunction'
+
+console.log(sentence.substr(30,24))
+
+//******* */ Exercises: Level 3*************************
+
+// Question-1 'Love is the best thing in this world. Some found their love and some are still looking for their love.' Count the number of word love in this sentence.
+
+let loveSentence ='Love is the best thing in this world. Some found their love and some are still looking for their love.' 
+
+console.log(loveSentence.match(/love/gi).length)
+
+//Question-2 Use match() to count the number of all because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
+
+console.log(sentence.match(/because/gi).length)
+
+// Question-3 Clean the following text and find the most frequent word (hint, use replace and regular expressions).
+
+    // const missSentence = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching'
+     
+    //  console.log(missSentence.match(/\W/g))
+    //  console.log(missSentence.replace(/[^A-Za-z\s]*/g,''))
+
+    var repetitiveWord = ""
+var timesWord = 0
+var crazySentence = "%I $am@% a %tea@cher%, &and& I lo%#ve %tea@ching%;. There $is nothing; &as& mo@re rewarding as educa@ting &and& @emp%o@wering peo@ple. ;I found tea@ching m%o@re interesting tha@n any other %jo@bs. %Do@es thi%s mo@tivate yo@u to be a tea@cher!?"
+var normalSentence = crazySentence.replace(/[^A-Za-z\s]*/g,'').toLowerCase();
+normalSentence.split(' ').forEach(word => {
+  var expression = new RegExp(`${word}`,'gi')
+  var matchedWord = normalSentence.match(expression)
+  if(matchedWord.length > timesWord){
+    repetitiveWord = word
+    timesWord = matchedWord.length
+  }
+});
+console.log(repetitiveWord)
+
+
+// Qusetion-3 Calculate the total annual income of the person by extracting the numbers from the following text. 'He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.'
+
+
+
+var salarySentence = "He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month."
+var gains = salarySentence.match(/[0-9]+/g)
+var totalAnnual = ( ( parseInt(gains[0]) + parseInt(gains[2]) ) * 12 ) + parseInt(gains[1])
+console.log(totalAnnual)

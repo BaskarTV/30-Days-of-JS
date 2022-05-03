a1114b66623c42c060e171ed3a4c3de8fab0a5e6
BASKAR T <baskar.tece@gmail.com>
2022-05-03 09:42:39 +0530

    Exercise continue

--- Exercises/Day-3/Day-3.js
+++ Exercises/Day-3/Day-3.js
@@ -36,3 +36,48 @@ console.log(parseInt('9.8'))
     // Write three JavaScript statement which provide truthy value.
 // Write three JavaScript statement which provide falsy value.
 
+console.log(4 >3)
+console.log(5 > 4)
+console.log(2 >1)
+console.log(3 >4)
+console.log(4 >5)
+console.log(1 >2)
+
+// Question-5 Figure out the result of the following comparison expression first without using console.log(). After you decide the result confirm it using console.log()
+
+/* i. 4 > 3  //true
+   ii. 4 >= 3 //true
+  iii. 4 < 3 //false
+  iv. 4 <= 3 //flase
+  v. 4 == 4  // true
+ vi. 4 === 4 //true
+vii. 4 != 4 //false
+viii. 4 !== 4 //false
+ix. 4 != '4' // true //false
+x. 4 == '4'  //true
+xi. 4 === '4' //true //false
+xii. Find the length of python and jargon and make a falsy comparison statement.*/
+
+
+console.log(1,4 > 3)
+console.log(2,4 >= 3)
+console.log(3,4 < 3)
+console.log(4,4 <= 3)
+console.log(5,4 == 4)
+console.log(6,4 === 4)
+console.log(7,4 != 4)
+console.log(8,4 !== 4)
+console.log(9,4 != '4')
+console.log(10,4 == '4')
+console.log(11,4 === '4')
+
+let py = 'python'
+let ja ='jargon'
+
+console.log(py.length)
+console.log(ja.length)
+console.log(py === ja)
+
+
+
+

987644dc1d95ce3ed4279bcc3137dd5af2bdc923
BASKAR T <baskar.tece@gmail.com>
2022-05-05 22:52:19 +0530

    Day 3 Exercises

--- Exercises/Day-3/Day-3.js
+++ Exercises/Day-3/Day-3.js
@@ -147,5 +147,41 @@ console.log('Promt of hegit and base')
 
 // Question-2 Write a script that prompt the user to enter side a, side b, and side c of the triangle and and calculate the perimeter of triangle 
 
+// let a = parseInt(prompt('Enter side a'))
+// let b = parseInt(prompt('Enter side b'));
+// let c =parseInt( prompt('Enter side c'));
 
+// let area = a + b + c
+
+// console.log("The perimeter of the triangle " + area)
+
+console.log("Perimeter of triangle method ")
+
+
+// Question-3 Get length and width using prompt and calculate an area of rectangle (area = length x width and the perimeter of rectangle (perimeter = 2 x (length + width))
+
+// const length = parseInt(prompt("Enter the length:"))
+// const width = parseInt(prompt("Enter the width"))
+
+// const areaoOfRect = length * width
+// const rectPerimeter = 2 * length + width
+
+// console.log('The Area of Rectangel is '+ areaoOfRect)
+// console.log('The perimeter of Rectangle is '+ rectPerimeter)
+
+console.log('Area of Rectangle');
+
+//Question-4   Get radius using prompt and calculate the area of a circle (area = pi x r x r) and circumference of a circle(c = 2 x pi x r) where pi = 3.14.
+
+
+// const pi =3.14;
+
+// const radius = parseInt(prompt('Enter the radius of circle:'))
+
+// const circleArea  = pi * radius * radius
+// const circumFrence = 2 * pi * radius
+
+// console.log("The area of Circle is "+ circleArea)
+// console.log('The Circumfrence of circle is '+ circumFrence)
+console.log('The area of Circumfrence')
 

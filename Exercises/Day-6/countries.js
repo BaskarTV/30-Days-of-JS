4e0bdacbc262d42f21991a571f733aff4ff1e439
BASKAR T <baskar.tece@gmail.com>
2022-06-04 23:15:02 +0530

    Day 6 completed

---
+++ Exercises/Day-6/countries.js
@@ -0,0 +1,14 @@
+export const countries = [
+  'Albania',
+  'Bolivia',
+  'Canada',
+  'Denmark',
+  'Ethiopia',
+  'Finland',
+  'Germany',
+  'Hungary',
+  'Ireland',
+  'Iceland',
+  'Japan',
+  'Kenya'
+]

803b66110501dda9687cf129108a2132ab982bab
BASKAR T <baskar.tece@gmail.com>
2022-06-06 23:07:43 +0530

    Day 7 Exercises

--- Exercises/Day-7/day7.js
+++ Exercises/Day-7/day7.js
@@ -157,7 +157,7 @@ break;
  return season
 }
 
-console.log(checkSeason('February')) 
+console.log('Season:',checkSeason('February')) 
 
 // 15. Math.max returns its largest argument. Write a function findMax that takes three arguments and returns their maximum with out using Math.max method.
 
@@ -175,3 +175,140 @@ console.log(findMax(0, -10, -2))
 
 // ******Exercise Level-2**************
 
+//1. Skipped
+
+// 2. Quadratic equation is calculated as follows: ax2 + bx + c = 0. Write a function which calculates value or values of a quadratic equation, solveQuadEquation.
+
+let solveQuadratic = function(a,b,c){
+ let disc = Math.pow(b, 2) -4*a*c
+
+ let posX = (-b+Math.pow(disc, 0.5))/(2*a);
+ let negX =(-b-Math.pow(disc, 0.5))/(2*a);
+ return [[posX, negX]]
+}
+
+console.log(solveQuadratic())
+console.log(solveQuadratic(1, 4, 4)) // {-2}
+console.log(solveQuadratic(1, -1, -2)) // {2, -1}
+console.log(solveQuadratic(1, 7, 12)) // {-3, -4}
+console.log(solveQuadratic(1, 0, -4)) //{2, -2}
+console.log(solveQuadratic(1, -1, 0)) //{1, 0}
+
+// 3.Declare a function name printArray. It takes array as a parameter and it prints out each value of the array.
+
+let arr = function printArray(arr){
+    return arr
+}
+
+console.log(arr(Array(10).fill(4)))
+
+// 4. Write a function name showDateTime which shows time in this format: 08/01/2020 04:08 using the Date object.
+
+let showDateTime = () =>{
+    let d = new Date()
+
+    let date = d.getDate();
+    let month = d.getMonth();
+    let year = d.getFullYear();
+    let hour = d.getHours();
+    let min = d.getMinutes ();
+
+    date = date <10 ? '0' + date.toString() : date;
+    month = month <10 ? '0' + month.toString() : month;
+    hour = hour < 10 ? '0' + hour.toString() : hour;
+    min = min <10 ? '0' + min.toString() : min
+
+    let dateTime = `${date}/${month}/${year} ${hour}:${min}`
+
+    return dateTime
+}
+
+console.log(showDateTime()) 
+
+// 5. Declare a function name swapValues. This function swaps value of x to y.
+
+function swapValues(x,y){
+  return [y,x]
+ 
+}
+   let x =3
+    let y=2
+
+console.log(swapValues(x,y))
+
+// 6 Declare a function name reverseArray. It takes array as a parameter and it returns the reverse of the array (don't use method).
+
+function reverseArray (arr){
+    let reversed = new Array;
+    for(let i = arr.length -1; i >= 0; i--){
+        reversed.push(arr[i])
+        
+    }
+    return reversed
+}
+console.log(reverseArray([1, 2, 3, 4, 5]))
+//[5, 4, 3, 2, 1]
+console.log(reverseArray(['A', 'B', 'C']))
+//['C', 'B', 'A']
+
+
+// 7. Declare a function name capitalizeArray. It takes array as a parameter and it returns the - capitalizedarray.
+
+function capitalizeArray (arr){
+    return arr.toUpperCase()
+}
+
+console.log(capitalizeArray('india'))
+
+// 8. Declare a function name addItem. It takes an item parameter and it returns an array after adding the item
+
+function addItem(item){
+    let arr = new Array;
+    arr.push(item)
+    return arr
+}
+console.log(addItem('HTML'))
+console.log(addItem('JavaScript'))
+
+// 9.Declare a function name removeItem. It takes an index parameter and it returns an array after removing an item
+
+function removeItem(arr, index){
+    arr.splice(index,1)
+    return arr
+}
+
+
+console.log(removeItem([0,1,2,3,4,5]))
+console.log(removeItem([1,2,3,4,5])) 
+console.log(removeItem([2,3,4,5]))
+console.log(removeItem([3,4,5]))
+console.log(removeItem([4,5]))
+console.log(removeItem([5]))
+
+// 10. Declare a function name sumOfNumbers. It takes a number parameter and it adds all the numbers in that range.
+
+function sumOfNumbers(num){
+    if(num == 0) return 0;
+    else  return num + sumOfNumbers(num -1)
+}
+
+console.log(sumOfNumbers(10))
+
+// 11.Declare a function name sumOfOdds. It takes a number parameter and it adds all the odd numbers in that - range.
+
+function sumOfOdds(num){
+    if(num <= 0) return 0;
+    num = num % 3 == 0 ? num -1 :num;
+    return num + sumOfOdds(num -3)
+}
+console.log(sumOfOdds(100))
+
+// 12. Declare a function name sumOfEven. It takes a number parameter and it adds all the even numbers in that - range.
+
+
+function sumOfEven(num){
+    if(num <= 0) return 0;
+    num = num % 2 == 0 ? num -1 :num;
+    return num + sumOfEven(num -2)
+}
+console.log(sumOfEven(100))
